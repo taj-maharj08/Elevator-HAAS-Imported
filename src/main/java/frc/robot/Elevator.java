38907743d85c79c8fdea8e9041fc29bf35d62bf5
taj-maharj08 <arjunmahajan@billericak12.com>
2023-11-14 18:41:54 -0500

    draft

---
+++ src/main/java/frc/robot/Elevator.java
@@ -0,0 +1,37 @@
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase{
+    private final CANSparkMax m_leftMotor, m_rightMotor;
+    private final double kDriveSpeed = 0.5;
+
+
+    public Elevator() {
+        m_leftMotor = new CANSparkMax(0, MotorType.kBrushless);
+        m_rightMotor = new CANSparkMax(1, MotorType.kBrushless);
+        setDefaultCommand(idle());
+    }
+    public Command extend() {
+        return this.runOnce(() -> {
+            m_leftMotor.set(kDriveSpeed);
+            m_rightMotor.set(kDriveSpeed);
+        }).withName("extend");
+    }
+    public Command idle() {
+        return this.runOnce(() -> {
+            m_leftMotor.set(0);
+            m_rightMotor.set(0);
+        }).withName("idle");
+    }
+    public Command retract() {
+        return this.runOnce(() -> {
+            m_leftMotor.set(-kDriveSpeed);
+            m_rightMotor.set(-kDriveSpeed);
+        }).withName("retract");
+    }
+}
---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,87 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
+  private final Elevator m_elevator;
+  private final CommandXboxController m_controller = new CommandXboxController(0);
+
+  public Robot() {
+    m_elevator = new Elevator();
+  }
+
+  @Override
+  public void robotInit() {
+    configureBindings();
+
+  }
+
+  @Override
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+    SmartDashboard.putString("Current Command", m_elevator.getCurrentCommand() != null ? m_elevator.getCurrentCommand().toString() : "");
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void disabledExit() {}
+
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = Commands.none();
+
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void autonomousExit() {}
+
+  @Override
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void teleopExit() {}
+
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void testExit() {}
+
+  private void configureBindings() {
+    m_controller.a().onTrue(m_elevator.extend()).onFalse(m_elevator.idle());
+    m_controller.b().onTrue(m_elevator.retract()).onFalse(m_elevator.idle());
+  }
+}
